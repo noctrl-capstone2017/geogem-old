b88ca4694fab16bb8399d96cf40d88960daebe1d
Kevin Morgan <kevismorgis@gmail.com>
2017-05-12 19:49:18 +0000

    Pear: Fixes some teacher testing problems

--- test/controllers/teachers_controller_test.rb
+++ test/controllers/teachers_controller_test.rb
@@ -2,18 +2,7 @@ require 'test_helper'
 
 class TeachersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    #Using setup here instead of fixtures because fixtures cause errors.
-    @teacher = Teacher.new(user_name: "profbill",
-                password_digest: "password",
-                last_login: Time.now,
-                full_name: "Professor Bill",
-                screen_name: "profbill",
-                icon: "apple",
-                color: "red",
-                email: "wtktriger@noctrl.edu",
-                description: "Super user",
-                powers: "Admin",
-                school_id: 0)
+    @teacher = teachers(:one)
   end
 
   test "should get index" do
