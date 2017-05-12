8c965f4dcee9c533d73701cd3e48c6d6731a6936
Steven Royster <steven.a.royster@gmail.com>
2017-05-12 19:40:12 +0000

    app_controller and new.html.erb update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  
+  protect_from_forgery with: :null_session
+  include LoginSessionHelper
+  
+  private
+
+    # Confirms a logged-in user.
+    def logged_in_teacher
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+  validates :teacher_password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :teacher_password_confirmation, presence: true, length: { minimum: 6 }, allow_nil: true
 end
