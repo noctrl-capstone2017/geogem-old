292aeb3590d69d7b020a7b89196216c96ac228f0
Matthew OBzera <mobzera516@gmail.com>
2017-05-14 04:12:21 +0000

    Fix merge conflicts

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,21 +1,7 @@
 # author: Kevin M, Tommy B
 # Teacher model validation.
 class Teacher < ApplicationRecord
-<<<<<<< HEAD
-
-  
-  #Creates the relationship of what students belong to the teacher
-  #author Matthew OBzera & Alex P
-  has_many :active_relationships, class_name:  "RosterStudent",
-                                  foreign_key: "teacher_id",
-                                  dependent:   :destroy
-
-  has_many :students, through: :active_relationships, source: :student 
-
-
-=======
-  
->>>>>>> 2b80b7e5c413d5d53fe8ebbc65463b4f9c8920bb
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   ###REGEX###
   #Only allows legit email formatting
@@ -29,8 +15,6 @@ class Teacher < ApplicationRecord
   
   ###VALIDAITONS###
   validates :user_name,  presence: true, length: { maximum: 75 }
-    #Commented out until login stuff is put in place. -KM
-  #validates :password_digest, presence: true, length: {minimum: 6 }
   
   validates :full_name, presence: true, length: { maximum: 75 }
   validates :screen_name, presence: true, length: { maximum: 8 },
@@ -50,19 +34,44 @@ class Teacher < ApplicationRecord
   validates :password_digest, presence: true, length: {minimum: 6 }
   has_secure_password
   
-  validates :teacher_password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :teacher_password_confirmation, presence: true, length: { minimum: 6 }, allow_nil: true
+  #Creates the relationship of what students belong to the teacher
+  #author Matthew OBzera + Alexander Pavia
+  has_many :active_relationships, class_name:  "RosterStudent",
+                                  foreign_key: "teacher_id",
+                                  dependent:   :destroy
+
+  has_many :students, through: :active_relationships, source: :student
+  
+  ### METHODS ###
+  # Returns a random token.
+  def Teacher.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = Teacher.new_token
+    update_attribute(:remember_digest, Teacher.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  # Returns the hash digest of the given string.
+  def Teacher.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
   
   private
 
-<<<<<<< HEAD
-
-=======
     # Converts email to all lower-case.
     def downcase_email
       self.email = email.downcase
     end
-    
->>>>>>> 2b80b7e5c413d5d53fe8ebbc65463b4f9c8920bb
 end
-
