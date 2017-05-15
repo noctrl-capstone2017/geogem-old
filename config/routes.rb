cfcbae8bbdef478f0d214d60889cfd32fc63b3a2
Taylor Spino <taspino@gmail.com>
2017-05-15 03:02:38 +0000

    Generated a reports controller, added seed for one session with 30 session events, and added the session summary pdf code

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :students
   resources :teachers
   resources :schools
+  get    '/report1',  to: 'reports#report1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # Login Session Controller
