edabae8cce410bb7714092216cb50f4fccb934db
Rolando M <rmedrano@noctrl.edu>
2017-05-16 14:55:37 +0000

    delted some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
   
   get    '/allSchools', to: 'schools#index'
   
-  get    '/notes',      to: 'session_notes#show'
+  #get    '/notes',      to: 'session_notes#show'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
