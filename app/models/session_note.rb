6065839916115782446b72b86c3a7bda9453afdc
Rolando M <rmedrano@noctrl.edu>
2017-05-17 20:33:08 +0000

    JS Form

--- app/models/session_note.rb
+++ app/models/session_note.rb
@@ -5,8 +5,5 @@ class SessionNote < ApplicationRecord
     validates :session_id, presence: true
     validates :note, presence: true
     
-    def student_for_note
-        @student = Student.find(full_name)
-        @session_note = SessionNote.create(created_at: Time.now)
-    end
+
 end
