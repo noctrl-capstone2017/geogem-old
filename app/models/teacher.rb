3c2b6708768ce5d110486532d3b254d9369c297b
Kevin Morgan <kevismorgis@gmail.com>
2017-05-21 20:38:16 +0000

    Teacher test fix

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -56,6 +56,7 @@ class Teacher < ApplicationRecord
   
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
+    return false if attribute.nil?
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
