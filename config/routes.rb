f1fa18163a36c9043ec8ca347b31835f2e54926e
Alexander Pavia <aapavia@noctrl.edu>
2017-05-21 00:02:53 +0000

    fixing end page, Be aware of test button on show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "/analysis", to: 'teachers#analysis'
   get 'static_pages/help'
   
-  get "sessions/end",to:'sessions#end_session'
+  #get "sessions/end",to:'sessions#end_session'
+  #get "sessions/:id/end",to:'sessions#end_session'
+  post 'sessions/:id/end_session' => 'sessions#end_session', as: :end_session
   
   #utilized http://stackoverflow.com/questions/25490308/ruby-on-rails-two-different-edit-pages-and-forms-how-to for help
   resources :teachers do
