6ce97b8d90f2350997b59f7988a89e2a9b67d90b
Kevin Morgan <kevismorgis@gmail.com>
2017-05-21 17:59:31 +0000

    Make new teacher draw from creating user's school_id

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -150,42 +150,6 @@ class TeachersController < ApplicationController
  
   private
   
-    #NOTE FROM CAROLYN C: Prof Bill and I moved these to the teachers_helper and included a TeacherHelper in here for navbar purposes.
-    
-    # Author: Steven Royster
-    # If the teacher is not an admin then they 
-    #  will be flashed an unauthorized prompt and redirected to home
-    # def is_admin
-    #   if !is_admin?
-    #     flash[:danger] = "Unauthorized"
-    #     redirect_to login_path
-    #   end
-    # end
-    
-    # Author: Steven Royster
-    # Checks to see if the current teacher has admin status
-    # Returns true if the teacher is an admin
-    # def is_admin?
-    #   current_teacher && current_teacher.powers == "Admin"
-    # end
-    
-    # Author: Steven Royster
-    # If the teacher is not a super user then they 
-    #  will be flashed an unauthorized prompt and redirected to home
-    # def is_super
-    #   if !is_super?
-    #     flash[:danger] = "Unauthorized"
-    #     redirect_to home1_path
-    #   end
-    # end
-
-     # Author: Steven Royster
-    # Checks to see if the current teacher has super user status
-    # Returns true if the teacher is a super user
-    # def is_super?
-    #   current_teacher && current_teacher.id == 1
-    # end
-    
     # Use callbacks to share common setup or constraints between actions.
     def set_teacher
       @teacher = Teacher.find(params[:id])
