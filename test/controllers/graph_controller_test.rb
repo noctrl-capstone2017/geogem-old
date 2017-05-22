2e706b1484d672aa462bef8f1f459e89b42e4ca8
Kevin Portland <hunter1209@comcast.net>
2017-05-22 20:40:52 +0000

    add tests

--- test/controllers/graph_controller_test.rb
+++ test/controllers/graph_controller_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
-
+require "net/http"
+#Kevin Portland
 class GraphControllerTest < ActionDispatch::IntegrationTest
   test "should get main" do
     get graph_main_url
@@ -24,6 +25,23 @@ class GraphControllerTest < ActionDispatch::IntegrationTest
   test "should get other" do
     get graph_other_url
     assert_response :success
+  end
+  
+  test "google should respond" do #https://stackoverflow.com/questions/5908017/check-if-url-exists-in-ruby
+    url = URI.parse("https://www.gstatic.com/charts/loader.js")
+  req = Net::HTTP.new(url.host, url.port)
+  req.use_ssl = (url.scheme == 'https')
+  path = url.path if url.path.present?
+  res = req.request_head(path || '/')
+  if res.kind_of?(Net::HTTPRedirection)
+    url_exist?(res['location']) # Go after any redirect and make sure you can access the redirected URL 
+  else
+    res.code[0] != "4" #false if http code starts with 4 - error on your side.
+  end
+rescue Errno::ENOENT
+  false #false if can't find the server
+end
+
   end
 
 end
