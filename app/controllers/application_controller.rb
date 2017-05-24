72ae059ae806b70781707f49165f5b35d3e2f16c
Kevin Portland <hunter1209@comcast.net>
2017-05-24 22:14:24 +0000

    reapply testing changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,20 +12,20 @@ class ApplicationController < ActionController::Base
   private
     # Confirms a logged-in user.
     def logged_in_teacher  
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+      # unless logged_in?
+      #   store_location
+      #   flash[:danger] = "Please log in."
+      #   redirect_to login_url
+      # end
     end
     
   private
     # Guard each page, checks for logged in
     def require_login  
-      unless current_teacher
-      flash[:danger] = "Log in is not current"
-        redirect_to login_url
-      end
+      # unless current_teacher
+      # flash[:danger] = "Log in is not current"
+      #   redirect_to login_url
+      # end
     end
     
 end
