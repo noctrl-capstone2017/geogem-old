54961a04c959b3f5a8b95b121820dab9570e6b2c
Dakota B <debrand@noctrl.edu>
2017-05-24 00:11:08 +0000

    Spring Cleaning

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,63 +1,57 @@
+# author: Dakota B and Robert H
+# Some school methods, as well as some super stuff.
+
 class SchoolsController < ApplicationController
   before_action :set_school, only: [:show, :edit, :update, :destroy]
 
-  # GET /schools
-  # GET /schools.json
+  # Used in the /schools route to display all schools
   def index
     @schools = School.paginate(page: params[:page], :per_page => 10)
   end
 
-  # GET /schools/1
-  # GET /schools/1.json
-  def show
-  end
-
-  # GET /schools/new
+  # Used in the creation of new schools
   def new
     @school = School.new
   end
 
-  # GET /schools/1/edit
-  def edit
-  end
-
-  # RH
-  # make the list of schools available at /super
+  # Used in the Super Dashboard to allow teacher 1 (profbill) to switch focus to any school
   def super
     @schools = School.all
     @teacher = Teacher.first
   end
   
-  def super_report
-  end
-  
+  # Used to pass information about which school will be backed up to the /backup page
   def backup
-    @schools = School.all
+    @current_teacher = current_teacher
+    @school = School.find(current_teacher.school_id)
+    @school_name = School.find(current_teacher.school_id).full_name
   end
   
+  # Used to pass information about which teacher at what school will be backed up to the /suspend page
   def suspend
     @current_teacher = current_teacher
-    #id = 1 written below refers to ProfBill, the SuperUser. He can't get deleted, and therefore will never be in the set of teachers elligible for deletion
+    # id = 1 written below refers to ProfBill, the SuperUser. He can't get deleted,
+    # and therefore will never be in the set of teachers elligible for deletion
     @teachers = Teacher.where(school_id: current_teacher.school_id).where.not(id: 1) 
     @school = School.find(current_teacher.school_id)
     @school_name = School.find(current_teacher.school_id).full_name
     @teacher_count = @teachers.count
   end 
 
+  # Used to pass information to the /restore page about which teachers at what school will be restored.
   def restore
     @current_teacher = current_teacher
-    @teachers = Teacher.where(school_id: current_teacher.school_id).where.not(id: 1) #id = 1 is ProfBill, the SuperUser. He can't get deleted.
+    #id = 1 is ProfBill, the SuperUser. He can't get deleted in the first place. No point in restoring.
+    @teachers = Teacher.where(school_id: current_teacher.school_id).where.not(id: 1) 
     @school = School.find(current_teacher.school_id)
     @school_name = School.find(current_teacher.school_id).full_name
     @teacher_count = @teachers.count
   end 
   
-
-  # POST /schools
-  # POST /schools.json
+  # Used in addition the the new method in the creation of schools.
   def create
     @school = School.new(school_params)
-
+    
     respond_to do |format|
       if @school.save
         format.html { redirect_to @school, notice: 'School was successfully created.' }
@@ -69,8 +63,7 @@ class SchoolsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /schools/1
-  # PATCH/PUT /schools/1.json
+  # Used to update a School profile
   def update
       if @school.update(school_params)
         redirect_to schools_path, :notice => "School updated"
@@ -79,28 +72,15 @@ class SchoolsController < ApplicationController
       end
   end
 
-
-  # DELETE /schools/1
-  # DELETE /schools/1.json
-  def destroy
-    @school.destroy
-    respond_to do |format|
-      format.html { redirect_to schools_url, notice: 'School was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
 
-   def set_school
+    def set_school
       @school = School.find(params[:id])
-   end
+    end
    
     # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
       params.require(:school).permit(:full_name, :screen_name, :icon, :color, :email, :website, :description)
     end
     
- 
-end
+end # end of controller
