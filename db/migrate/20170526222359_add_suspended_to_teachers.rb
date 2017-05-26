e426b40b5536552cd1ae1cc8d61a9b5f573cbcee
Kevin Morgan <kevismorgis@gmail.com>
2017-05-26 22:46:10 +0000

    Fix roster page adding/removal

--- db/migrate/20170526222359_add_suspended_to_teachers.rb
+++ db/migrate/20170526222359_add_suspended_to_teachers.rb
@@ -1,4 +1,4 @@
-class AddSuspensionToTeachers < ActiveRecord::Migration[5.0]
+class AddSuspendedToTeachers < ActiveRecord::Migration[5.0]
   def change
     add_column :teachers, :suspended, :boolean, :null => false, :default => false
   end
