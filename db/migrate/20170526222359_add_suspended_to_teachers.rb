429dc66db68660165170ac45abf6471ec83a896f
Kevin Morgan <kevismorgis@gmail.com>
2017-05-26 22:27:40 +0000

    Suspended migration fix

--- db/migrate/20170526222359_add_suspended_to_teachers.rb
+++ db/migrate/20170526222359_add_suspended_to_teachers.rb
@@ -1,5 +1,5 @@
 class AddSuspensionToTeachers < ActiveRecord::Migration[5.0]
   def change
-    add_column :teachers, :suspended, :boolean
+    add_column :teachers, :suspended, :boolean, :null => false, :default => false
   end
 end
